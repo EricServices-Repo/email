af1f20b5ccbf8a45398947374d5e4e69ef2f2665
eembling <eembling@gmail.com>
2023-11-09 21:18:19 -0500

    Update email-server-install.sh

--- email-server-install.sh
+++ email-server-install.sh
@@ -184,10 +184,8 @@ firewall-cmd --reload
 #####################
 echo -e "Change required values for Postfix\n"
 
-myhostname=
-sed -i 's/#host: \"localhost:5601\"/host: \"'"${KIBANA}"':5601\"/' /etc/metricbeat/metricbeat.yml
-
-
+#myhostname=
+#sed -i 's/#host: \"localhost:5601\"/host: \"'"${KIBANA}"':5601\"/' /etc/metricbeat/metricbeat.yml
 #sed -i 's/#host: \"localhost:5601\"/host: \"'"${KIBANA}"':5601\"/' /etc/metricbeat/metricbeat.yml
 
 
