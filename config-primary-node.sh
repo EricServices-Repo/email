8930fd8fbf2baea84fbc3b14e209f1f994599077
eembling <eembling@gmail.com>
2023-11-25 13:24:30 -0500

    Update config-primary-node.sh

--- config-primary-node.sh
+++ config-primary-node.sh
@@ -4,8 +4,42 @@
 # Sets up Primary Mail Node for MySQL Replication
 #
 ###############################################################
-Version 1.0.1
+# Version 1.0.1
 #
 # 
 # 
 ################################################################
+
+#################
+# Define Colors #
+#################
+RED="\e[31m"
+GREEN="\e[32m"
+BLUE="\e[34m"
+ENDCOLOR="\e[0m"
+
+
+echo -e "${GREEN}EricServic.es Email Server Primary Setup${ENDCOLOR}"
+
+echo -e "${BLUE} ______      _       _____                 _                    ${ENDCOLOR}"  
+echo -e "${BLUE}|  ____|    (_)     / ____|               (_)                   ${ENDCOLOR}"
+echo -e "${BLUE}| |__   _ __ _  ___| (___   ___ _ ____   ___  ___   ___  ___    ${ENDCOLOR}"
+echo -e "${BLUE}|  __| | '__| |/ __|\___ \ / _ \ '__\ \ / / |/ __| / _ \/ __|   ${ENDCOLOR}"
+echo -e "${BLUE}| |____| |  | | (__ ____) |  __/ |   \ V /| | (__ |  __/\__ \   ${ENDCOLOR}"
+echo -e "${BLUE}|______|_|  |_|\___|_____/ \___|_|    \_/ |_|\___(_)___||___/ \n${ENDCOLOR}"
+
+
+#####################
+# Set all Variables #
+#####################
+echo -e "${GREEN}Set Variables for custom install.${ENDCOLOR}"
+
+read -p "Use EricServic.es Repository [y/N]:" ESREPO
+ESREPO="${ESREPO:=n}"
+echo "$ESREPO"
+
+
+
+
+
+
