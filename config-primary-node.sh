f63f287ad10d02614a8a4d20e7be121562b5fa02
eembling <eembling@gmail.com>
2023-11-25 13:48:19 -0500

    Update config-primary-node.sh

--- config-primary-node.sh
+++ config-primary-node.sh
@@ -34,12 +34,33 @@ echo -e "${BLUE}|______|_|  |_|\___|_____/ \___|_|    \_/ |_|\___(_)___||___/ \n
 #####################
 echo -e "${GREEN}Set Variables for custom install.${ENDCOLOR}"
 
-read -p "Use EricServic.es Repository [y/N]:" ESREPO
-ESREPO="${ESREPO:=n}"
-echo "$ESREPO"
+read -p "Secondary node IP Address [192.168.1.2]:" SECONDARYIPADDRESS
+SECONDARYIPADDRESS="${SECONDARYIPADDRESS:=192.168.1.2}"
+echo "$SECONDARYIPADDRESS"
+
+read -p "MySQL Password [password]:" SQLPASSWORD
+SQLPASSWORD="${SQLPASSWORD:=password}"
+echo "$SQLPASSWORD"
+
+read -p "MySQL Password [password]:" REPLICATIONPASSWORD
+REPLICATIONPASSWORD="${REPLICATIONPASSWORD:=password}"
+echo "$REPLICATIONPASSWORD"
 
 
+echo -e "${GREEN}Configure MySQL Replication\n${ENDCOLOR}"
+mysql --user=root --password=$SQLPASSWORD -e "CREATE USER replication@'%' identified by '$REPLICATIONPASSWORD';"
+mysql --user=root --password=$SQLPASSWORD -e "GRANT REPLICATION SLAVE ON *.* TO replication@'%';"
+mysql --user=root --password=$SQLPASSWORD -e "FLUSH PRIVILEGES;"
+mysql --user=root --password=$SQLPASSWORD -e "FLUSH TABLES WITH READ LOCK;"
+mysql --user=root --password=$SQLPASSWORD -e "UNLOCK TABLES;"
 
+echo -e "Configure MySQL Firewall for Secondary Node\n"
+firewall-cmd --new-zone=mariadb-access --permanent
+firewall-cmd --zone=mariadb-access --add-source=$SECONDARYIPADDR/32 --permanent
+firewall-cmd --zone=mariadb-access --add-port=3306/tcp  --permanent
+firewall-cmd --reload
 
+echo -e "Reconfigure MySQL Listening Interface\n"
+sed -i "s/#bind-address=0.0.0.0/bind-address=0.0.0.0/" /etc/my.cnf.d/mariadb-server.cnf
 
-
+sed -i '/\[mysqld\]/a server-id=1\nlog_bin = \/var\/log\/mysql\/mysql-bin.log\nbinlog-format=ROW' /etc/my.cnf.d/mariadb-server.cnf
