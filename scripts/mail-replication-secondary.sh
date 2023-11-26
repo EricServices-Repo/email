781cbbf48ec069f3a1fe46650ba1d56b8878d517
eembling <eembling@gmail.com>
2023-11-25 20:09:22 -0500

    Update mail-replication-secondary.sh

--- scripts/mail-replication-secondary.sh
+++ scripts/mail-replication-secondary.sh
@@ -1,1 +1,11 @@
-
+#!/usr/bin/env bash
+# EricServic.es Email Server Configure Primary Node
+#
+# Sets up Primary Mail Node for MySQL Replication
+#
+###############################################################
+# Version 1.0.1
+#
+# 
+# 
+################################################################
