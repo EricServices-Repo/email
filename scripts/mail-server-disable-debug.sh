a91ef01cd9fcde5811c4543d9f1dd92e2ba67d69
eembling <eembling@gmail.com>
2023-11-25 20:13:20 -0500

    Update mail-server-disable-debug.sh

--- scripts/mail-server-disable-debug.sh
+++ scripts/mail-server-disable-debug.sh
@@ -1,1 +1,14 @@
+#!/usr/bin/env bash
+echo -e "${GREEN}Disabling Mail Server Debugs\n${ENDCOLOR}"
+sed -i 's/auth_verbose = yes/#auth_verbose = no/' /etc/dovecot/conf.d/10-logging.conf
+sed -i 's/auth_verbose_passwords = yes/#auth_verbose_passwords = no/' /etc/dovecot/conf.d/10-logging.conf
+sed -i 's/auth_debug = yes/#auth_debug = no/' /etc/dovecot/conf.d/10-logging.conf
+sed -i 's/auth_debug_passwords = yes/#auth_debug_passwords = no/' /etc/dovecot/conf.d/10-logging.conf
+sed -i 's/mail_debug = yes/#mail_debug = no/' /etc/dovecot/conf.d/10-logging.conf
+sed -i 's/verbose_ssl = yes/#verbose_ssl = no/' /etc/dovecot/conf.d/10-logging.conf
 
+sed -i 's/debug_peer_level = 6/debug_peer_level = 2/' /etc/postfix/main.cf
+sed -i "s/debug_peer_list =.*/#debug_peer_list = some.domain/" /etc/postfix/main.cf
+systemctl restart dovecot
+systemctl restart postfix
+echo -e "Dovecot and Postfix debugs disabled\n"
