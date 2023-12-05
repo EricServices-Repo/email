f91c99b3208f39f7fa4fd087c9c4e722b1446895
eembling <eembling@gmail.com>
2023-12-05 10:22:55 -0500

    Update mail-update.sh

--- scripts/mail-update.sh
+++ scripts/mail-update.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 echo -e "Running Mail Scripts Updater"
-#Update the script for next time
-wget -P /opt/mail-scripts https://raw.githubusercontent.com/EricServices-Repo/mail/main/scripts/mail-update.sh
-chmod +x /opt/mail-scripts/mail-update.sh
+# Update the script for next time
+wget -P /opt/mail-scripts https://raw.githubusercontent.com/EricServices-Repo/mail/main/scripts/mail-update.sh.new
 
-
+# Downloading all current scripts
 wget -P /opt/mail-scripts https://raw.githubusercontent.com/EricServices-Repo/mail/main/scripts/mail-add-domain.sh
 wget -P /opt/mail-scripts https://raw.githubusercontent.com/EricServices-Repo/mail/main/scripts/mail-replication-primary.sh
 wget -P /opt/mail-scripts https://raw.githubusercontent.com/EricServices-Repo/mail/main/scripts/mail-replication-secondary.sh
@@ -12,6 +11,7 @@ wget -P /opt/mail-scripts https://raw.githubusercontent.com/EricServices-Repo/ma
 wget -P /opt/mail-scripts https://raw.githubusercontent.com/EricServices-Repo/mail/main/scripts/mail-server-enable-debug.sh
 wget -P /opt/mail-scripts https://raw.githubusercontent.com/EricServices-Repo/mail/main/scripts/mail-help.sh
 
+# Allowing Script to run
 chmod +x /opt/mail-scripts/mail-add-domain.sh
 chmod +x /opt/mail-scripts/mail-replication-primary.sh
 chmod +x /opt/mail-scripts/mail-replication-secondary.sh
@@ -19,7 +19,7 @@ chmod +x /opt/mail-scripts/mail-server-disable-debug.sh
 chmod +x /opt/mail-scripts/mail-server-enable-debug.sh
 chmod +x /opt/mail-scripts/mail-help.sh
 
-
+# Update Alias Commands
 cat << EOF >> ~/.bashrc
 alias maildebug='sh /opt/mail-scripts/mail-server-enable-debug.sh'
 alias mailundebug='sh /opt/mail-scripts/mail-server-disable-debug.sh'
@@ -30,4 +30,9 @@ alias mailhelp='sh /opt/mail-scripts/mail-help.sh'
 alias mailupdate='sh /opt/mail-scripts/mail-update.sh'
 EOF
 
-echo -e " Mail Scripts Updated"
+# Move in newly downloaded script.
+rm /opt/mail-scripts/mail-update.sh
+mv /opt/mail-scripts/mail-update.sh.new /opt/mail-scripts/mail-update.sh
+chmod +x /opt/mail-scripts/mail-update.sh
+
+echo -e " Mail Scripts Updated, reboot needed for alias commands"
